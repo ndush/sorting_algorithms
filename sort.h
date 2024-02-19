48af2dd204ec3c4c192f8bccac0176d6f3840b25
ndush <mdamarisnduku@gmail.com>
2024-02-19 08:46:17 -0800

    2

--- sort.h
+++ sort.h
@@ -41,5 +41,5 @@ void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 listint_t *create_listint(const int *array, size_t size);
-
+void selection_sort(int *array, size_t size);
 #endif /* SORT_H */
