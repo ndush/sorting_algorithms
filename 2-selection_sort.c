0e8291b74a6a9cc8064dcc232f23642ef9da1b8a
ndush <mdamarisnduku@gmail.com>
2024-02-19 08:50:14 -0800

    2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,32 +9,33 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
+	size_t i, j, min_idx;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    i = 0;
-    j = 0;
-    min_idx = 0;
+	i = 0;
+	j = 0;
+	min_idx = 0;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
 
-        if (min_idx != i)
-        {
-            int temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
-            print_array(array, size);
-        }
-    }
+		if (min_idx != i)
+		{
+			int temp = array[i];
+
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
 }
 
