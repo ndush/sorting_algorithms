ba8e03faeef0cd3aa5e775a55ec3e3dcd540ee77
ndush <mdamarisnduku@gmail.com>
2024-02-22 00:24:34 -0800

    m

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -14,67 +14,68 @@
  *              sub-arrays, each of which is sorted recursively, and then
  *              merged to produce a single sorted array.
  */
+void j(void);
 void merge_sort(int *array, size_t size)
 {
-    if (size > 1)
-    {
-        size_t mid = size / 2;
-        size_t i, j, k;
-        int *left = array;
-        int *right = array + mid;
-        size_t left_size = mid;
-        size_t right_size = size - mid;
-        int *temp = malloc(size * sizeof(int));
+	if (size > 1)
+	{
+		size_t mid = size / 2;
+		size_t i, j, k;
+		int *left = array;
+		int *right = array + mid;
+		size_t left_size = mid;
+		size_t right_size = size - mid;
+		int *temp = malloc(size * sizeof(int));
 
-        if (temp == NULL)
-        {
-            perror("malloc");
-            exit(EXIT_FAILURE);
-        }
+		if (temp == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
 
-        merge_sort(left, left_size);
-        merge_sort(right, right_size);
+		merge_sort(left, left_size);
+		merge_sort(right, right_size);
 
-        printf("Merging...\n[left]: ");
-        print_array(left, left_size);
-        printf("[right]: ");
-        print_array(right, right_size);
+		printf("Merging...\n[left]: ");
+		print_array(left, left_size);
+		printf("[right]: ");
+		print_array(right, right_size);
 
-        i = 0;
-        j = 0;
-        k = 0;
+		i = 0;
+		j = 0;
+		k = 0;
 
-        while (i < left_size && j < right_size)
-        {
-            if (left[i] <= right[j])
-            {
-                temp[k++] = left[i++];
-            }
-            else
-            {
-                temp[k++] = right[j++];
-            }
-        }
+		while (i < left_size && j < right_size)
+		{
+			if (left[i] <= right[j])
+			{
+				temp[k++] = left[i++];
+			}
+			else
+			{
+				temp[k++] = right[j++];
+			}
+		}
 
-        while (i < left_size)
-        {
-            temp[k++] = left[i++];
-        }
+		while (i < left_size)
+		{
+			temp[k++] = left[i++];
+		}
 
-        while (j < right_size)
-        {
-            temp[k++] = right[j++];
-        }
+		while (j < right_size)
+		{
+			temp[k++] = right[j++];
+		}
 
-        for (i = 0; i < size; i++)
-        {
-            array[i] = temp[i];
-        }
+		for (i = 0; i < size; i++)
+		{
+			array[i] = temp[i];
+		}
 
-        printf("[Done]: ");
-        print_array(array, size);
+		printf("[Done]: ");
+		print_array(array, size);
 
-        free(temp);
-    }
+		free(temp);
+	}
 }
 
